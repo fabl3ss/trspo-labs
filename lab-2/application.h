7402c87ee86a633a6acf3d9a071ad59d864a558a
Maksym <maxym.chaika@gmail.com>
2022-09-28 23:35:26 +0300

    Second homework

---
+++ lab-2/application.h
@@ -0,0 +1,39 @@
+#include <mutex>
+#include <random>
+#include <thread>
+#include <functional>
+
+#include "encapsulated.h"
+
+#define MIN_THREADS_AMOUNT 10
+#define MAX_THREADS_AMOUNT 20
+#define MIN_ITERATIONS_AMOUNT 10000
+#define MAX_ITERATIONS_AMOUNT 20000
+
+class Application
+{
+	std::mutex _mtx;
+	std::mt19937 _mt;
+	std::random_device _rd;
+
+	std::vector<std::thread> _workers;
+
+	unsigned int _workersAmount;
+	unsigned int _firstWorkIterations;
+	unsigned int _secondWorkIterations;
+
+	EncapsulatedFloatWithMutex* a;
+	EncapsulatedFloatWithMutex* b;
+
+	void firstJob(size_t iters);
+	void secondJob(size_t iters);
+	void createFirstJobThreads(unsigned int amount);
+	void createSecondJobThreads(unsigned int threadsAmount);
+	void printJobStatus(std::string workName);
+	void createWorkers();
+	void waitAllWorkers();
+public:
+	Application();
+	void Run();
+};
+
