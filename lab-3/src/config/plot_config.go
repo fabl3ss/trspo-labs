15bbd08d6f3d27c3566f2643ff6ce772a04599c9
Maksym <maxym.chaika@gmail.com>
2022-10-20 03:00:26 +0300

    Implemented multithreaded collatz conjecture

---
+++ lab-3/src/config/plot_config.go
@@ -0,0 +1,12 @@
+package config
+
+import "image/color"
+
+const (
+	PlotHeight   = 4 * 72
+	PlotWidth    = 10 * 72
+	PlotTitle    = "Maksym Chaika KI-03 Collatz Conjecture"
+	PlotFilename = "Collatz_Conjecture"
+)
+
+var PlotBackground = color.NRGBA{R: 200, G: 200, B: 200, A: 255}
